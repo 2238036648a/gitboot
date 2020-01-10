33594527c447de054d8ceee405e4bb9dcd643c4c
andonghong <2238036648@qq.com>
2020-01-10 22:05:25 +0800

    llll

---
+++ mail/src/main/java/com/mr/adh/ApplicationStart.java
@@ -0,0 +1,19 @@
+package com.mr.adh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ApplicationStart
+ * @Description: TODO
+ * @Author adh
+ * @Date 2020/1/10
+ * @Version V1.0
+ **/
+@SpringBootApplication
+public class ApplicationStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class);
+    }
+}
